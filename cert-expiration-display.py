47e2f40274d67e06af9cc08d78b02675934b53c1
Pierce Ortlieb <po@PO-MacBook.local>
2017-10-16 12:31:52 -0400

    Python script that will prompt user for server name and specific port they want to find the SSL cert expiration info of.

---
+++ cert-expiration-display.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Author: Pierce Ortlieb
+# Date: 6/19/17
+# Title: cert-expiration-display.py
+# About: Script runs a openssl chained command to display certificate expiration info, the user will have the option to input the desired servername and port
+import subprocess 
+from subprocess import Popen, PIPE
+
+print("------ Cert Depth & Expiration Display v1.0  ------")
+server_name = raw_input("Enter server name: ")
+port_number = raw_input("Enter port number: ")
+
+def display_cert_info(servername, portnum):
+	# translate parameters to String for Unix commands
+	pn = str(server_name + ":" + port_number)
+	
+	# Unix commands we want to run
+	cmd1 = ["openssl", "s_client", "-servername", str(servername), "-connect", pn]
+	cmd2 = ["openssl", "x509", "-noout", "-dates"]
+	
+	# Use subprocess to pipe our commands through one another
+	proc1 = subprocess.Popen(cmd1, stdout=PIPE)
+	proc2 = subprocess.Popen(cmd2, stdin=proc1.stdout, stdout=PIPE) # Set stdin to the stdout of subproc you want output from
+	
+	print(proc2.communicate()[0])
+	print("-----------------------------------------------------------------")
+	
+	
+display_cert_info(server_name, port_number)	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
